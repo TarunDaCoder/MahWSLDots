c146bf30df3bc957cea151a4b77fd1d041c58761
TarunDaCoder <tarunsnair09@gmail.com>
2021-09-19 08:36:55 +0530

    I'm never gonna complete this

--- dev/CodeProjects/Face_Recognition_App/app.js
+++
@@ -1,9 +0,0 @@
-const video = document.getElementById('video')
-function startVideo(){
-    navigator.getUserMedia(
-        {video: {}},
-        stream => video.srcObject = stream,
-        err => console.error(err)
-    )
-}
-startVideo()
